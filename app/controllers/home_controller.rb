0a5a2198b4a7f8ad745812a5766e9f7c15b4afbf
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-05-02 19:34:34 +0000

    ADDED LIST OF BRUSHES to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,10 @@ class HomeController < ApplicationController
 
     @list_of_programs = matching_programs.order({ :created_at => :desc })
 
+    matching_brushes = Brush.all
+
+    @list_of_brushes = matching_brushes.order({ created_at: :desc })
+
     render({ template: "homepage/index.html.erb" })
   end
 end
